3272bff1a664fdc47d52d2a4035e0a0f709c895c
Eli Talcott <eli.talcott@gmail.com>
2020-03-14 17:45:19 -0400

    Random seat assignment finished

--- main.py
+++ main.py
@@ -8,14 +8,62 @@ import sys
 import random
 import excel_io
 
-CLUBS = 0
-DIAMONDS = 1
+CLUBS = 1
 HEARTS = 2
 SPADES = 3
+DIAMONDS = 4
+
+###
+## Number of students per table dictionary and algorithm
+###
+seats = {9: (0, 0, 3, 3, 0, 3, 0), 10: (0, 0, 4, 3, 0, 3, 0), 11: (0, 0, 4, 4, 0, 3, 0),
+         12: (3, 0, 3, 3, 0, 3, 0), 13: (4, 0, 3, 3, 0, 3, 0), 14: (4, 0, 4, 3, 0, 3, 0),
+         15: (3, 3, 0, 3, 0, 3, 3), 16: (4, 3, 0, 3, 0, 3, 3), 17: (4, 3, 0, 4, 0, 3, 3),
+         18: (3, 3, 3, 0, 3, 3, 3), 19: (3, 3, 4, 0, 3, 3, 3), 20: (3, 3, 4, 3, 4, 3, 0),
+         21: (3, 3, 3, 3, 3, 3, 3), 22: (3, 3, 4, 3, 3, 3, 3), 23: (3, 4, 4, 3, 3, 3, 3),
+         24: (4, 4, 4, 3, 3, 3, 3), 25: (4, 4, 4, 4, 3, 3, 3), 26: (4, 3, 4, 4, 3, 4, 4),
+         27: (4, 4, 4, 3, 4, 4, 4), 28: (4, 4, 4, 4, 4, 4, 4)}
+
+def seat_options(tables):
+    cards = []
+    for i, table in enumerate(tables):
+        if table == 3:
+            cards.append((str(i + 1)) + " of clubs")
+            cards.append((str(i + 1)) + " of hearts")
+            cards.append((str(i + 1)) + " of spades")
+        elif table == 4:
+            cards.append((str(i + 1)) + " of clubs")
+            cards.append((str(i + 1)) + " of hearts")
+            cards.append((str(i + 1)) + " of spades")
+            cards.append((str(i + 1)) + " of diamonds")
+    return cards
+
+
+###
+## Randomly assign seats
+###
+def assign_seats(students, cards):
+    assignments = {}
+    for i, student in enumerate(students):
+        num_remaining = len(students) - i
+        rand = random.randint(0, num_remaining - 1)
+        assignments[student] = cards.pop(rand)
+    return assignments
+
 
 if __name__ == "__main__":
     if len(sys.argv) == 2:
+        # Read class list from Excel
         class_list = excel_io.read_class_lists(sys.argv[1])
-        print(class_list)
+
+        # Get deck of cards for seating chart
+        deck = seat_options(seats[len(class_list)])
+
+        # Assign a random card to each student
+        seat_assignments = assign_seats(class_list, deck)
+
+        # Print assignments
+        for key in seat_assignments:
+            print(key + ": " + seat_assignments[key])
     else:
         sys.exit("Wrong arguments!")
